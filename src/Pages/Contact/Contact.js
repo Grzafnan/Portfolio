ab6d2e8881e130513fdde323fb2ac7841aa0b528
grzafnan <grzafnan@gmail.com>
2022-12-10 23:27:19 +0600

    added email functionality

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import contact from '../../assets/contact.svg'
+import emailjs from '@emailjs/browser';
+import toast from 'react-hot-toast';
 
 const Contact = () => {
+  const form = useRef();
+  console.log(process.env.REACT_APP_publicKey);
+  const handleMessage = event => {
+    event.preventDefault();
+    emailjs.sendForm(`${process.env.REACT_APP_serviceId}`, `${process.env.REACT_APP_templateID}`, form.current, `${process.env.REACT_APP_publicKey}`)
+      .then((result) => {
+        console.log(result.text);
+        toast.success('Email sent successfully');
+      }, (error) => {
+        console.log(error.text);
+        toast.error(error.text);
+      });
+  }
+
+
   return (
     <section id='contact' className='flex flex-col lg:flex-row justify-between gap-10 items-center py-14 px-6'>
       <div className='w-1/2'>
@@ -15,9 +32,9 @@ const Contact = () => {
           </h3>
         </div>
         <div className=''>
-          <form action="" className='flex flex-col gap-5'>
-            <input type="text" class="py-3 px-4 block w-full bg-accent border border-darkGray rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" placeholder="Name" required />
-            <input type="email" name="email" id="email" class="py-3 px-4 block w-full bg-accent border border-darkGray rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" placeholder="Email" />
+          <form ref={form} onSubmit={handleMessage} className='flex flex-col gap-5'>
+            <input type="text" name="user_name" id='name' class="py-3 px-4 block w-full bg-accent border border-darkGray rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" placeholder="Name" required />
+            <input type="email" name="user_email" id="email" class="py-3 px-4 block w-full bg-accent border border-darkGray rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400" placeholder="Email" />
 
             <textarea className='py-3 px-4 block w-full bg-accent border border-darkGray rounded-md text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400' name="message" id="message" cols="20" rows="6" placeholder='Message...' required></textarea>
             <button
